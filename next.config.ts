f3725fa7ca82eb92a1d0f5ec07ec4317380ada41
Julian Montes Ramirez <damon.198623@gmail.com>
2025-07-18 20:26:19 +0000

    revisa que no me deja poner este enlace en la entrada en la tabla https:

--- next.config.ts
+++ next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'btwhvavwqkzifiuhgcao.supabase.co',
         port: '',
-        pathname: '/**',
+        pathname: '/storage/v1/object/**',
       }
     ],
   },
