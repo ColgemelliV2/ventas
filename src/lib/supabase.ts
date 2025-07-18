2c2f74c61aa47fd761235e1dcf9c208e36dfd059
Julian Montes Ramirez <damon.198623@gmail.com>
2025-07-18 21:31:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://btwhvavwqkzifiuhgcao.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0d2h2YXZ3cWt6aWZpdWhnY2FvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI4NTQ0MjYsImV4cCI6MjA2ODQzMDQyNn0.LZnCkY7DkbA-3K9TWOjn23pzDC6ye9Kvl8L8qZsW1Ds';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL and Anon Key are required.');
